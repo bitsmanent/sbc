2d61e19da96ca59e0cd196d8892d31922154a73a
Claudio Alessi <smoppy@gmail.com>
2022-04-13 10:24:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+simple base converter
+=====================
+sbc is a tool to convert between bases. The given value is converted and
+printed in decimal, hexadecimal, octal and binary.
+
+Status
+------
+sbc is considered complete and no further development is expected to happen.
---
+++ sbc.c
@@ -0,0 +1,39 @@
+/* simple base converter */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char *argv[]) {
+	unsigned long n;
+	int len, base = 0, i, s;
+
+	if(argc < 2 || argc > 3) {
+		printf("Usage: %s <value> [base]\n", argv[0]);
+		return 1;
+	}
+	if(argv[2])
+		base = atoi(argv[2]);
+	else {
+		len = strlen(argv[1]);
+		if(argv[1][len-1] == 'b' && argv[1][1] != 'x')
+			base = 2;
+	}
+	n = strtoul(argv[1], NULL, base);
+	if(errno == ERANGE || errno == EINVAL)
+		return -1;
+	printf("%lu %#04lx %lo ", n, n, n);
+	for(s = 64 - 8; s > 0 && !(n >> s); s -= 8);
+	for(i = 0; i < 8 && s >= 0; i++) {
+		printf("%d", ((n >> s) << i) & 0x80 ? 1 : 0);
+		if(i == 8 - 1) {
+			s -= 8;
+			i = -1;
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return 0;
+}
